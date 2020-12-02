1263a9af39ed1a3cf56cb1f923001683a675d637
thanhnh-infinity <thanhnh2911@gmail.com>
2020-12-02 15:31:40 -0700

    try to run 1st

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/thanhnh/Development_Env/SDK/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/thanhnh/Workspaces/All_Workspace/Flutter_Workspace/flutter-to-do-list"
+export "FLUTTER_TARGET=/Users/thanhnh/Workspaces/All_Workspace/Flutter_Workspace/flutter-to-do-list/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/Users/thanhnh/Development_Env/SDK/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=flutter.inspector.structuredErrors%3Dtrue"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
